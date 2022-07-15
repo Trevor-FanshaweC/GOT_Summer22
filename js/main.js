19eac1ff85f94199b7e9cd0d6eeed6a0053711b0
Van Rys <tvanrys@ats-hr420-dmpod.fanshawec.ca>
2022-07-15 13:23:22 -0400

    dynamic text / fip ref

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 (() => {
   // variables / DOM connections go here
-  let   sigils = document.querySelectorAll('.sigilContainer'),
+    let sigils = document.querySelectorAll('.sigilContainer'),
         houseVid = document.querySelector('video'),
         lightBox = document.querySelector('.lightbox'),
-        closeBut = lightBox.querySelector('.close');
+        closeBut = lightBox.querySelector('.close'),
+        houseName = document.querySelector('.house-name'),
+        houseInfo = document.querySelector('.house-desc');
+
+    const houseData = [
+        [`stark`, `stark house info`], // index 0 => houseData[0][0], houseData[0][1]
+        [`baratheon`, `baratheon house info`], // index 1
+        [`lannister`, `lannister house info`], // index 2
+        [`tully`, `tully house info`], // index 3
+        [`greyjoy`, `greyjoy house info`], // index 4
+        [`arryn`, `arryn house info`]// index 5
+    ];
+
+    function showHouseData() {
+        //debugger;
+        // show the right house info on a sigil click
+        // this refers to the shield you clicked on
+        // this.dataset.offset is the data-offset attribute defined on that html tag
+        houseName.textContent = `House ${houseData[this.dataset.offset][0]}`;
+        houseInfo.textContent = houseData[this.dataset.offset][1];
+    }
 
     function openLightbox() {
         lightBox.classList.add('show-lightbox');
@@ -18,7 +38,9 @@
     }
 
     // event handling here (how is the user going to interact with stuff)
-    sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+    //sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+
+    sigils.forEach(sigil => sigil.addEventListener('click', showHouseData));
 
     houseVid.addEventListener('ended', closeLightbox);
     closeBut.addEventListener('click', closeLightbox);
